60db0e35b9fc19c99190796fca1dd671d7e1519b
dangphuchuu <phuchuu0120@gmail.com>
2023-04-30 16:41:44 +0700

    30/4/2023

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -18,7 +18,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link " href="./pages/tables.html">
+                <a class="nav-link " href="./admin/list_movie">
                     <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="fa-solid fa-film text-warning text-sm opacity-10"></i>
                     </div>
--- resources/views/admin/list_movie/list.blade.php
+++ resources/views/admin/list_movie/list.blade.php
@@ -9,7 +9,7 @@
         </div>
         <div class="card-body px-0 pt-0 pb-2">
           <div class="table-responsive p-0">
-            <a href="admin/movie_genres/create" style="float:right;padding-right:30px;" class="text-light">
+            <a href="admin/list_movie/create" style="float:right;padding-right:30px;" class="text-light">
               <button class=" btn btn-primary float-right mb-3">Create</button>
             </a>
             <table class="table align-items-center mb-0 ">
