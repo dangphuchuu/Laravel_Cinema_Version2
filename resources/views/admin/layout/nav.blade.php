3d9a1d0233546401009d1c67c5f08b574114b401
dangphuchuu <phuchuu0120@gmail.com>
2023-05-09 13:58:00 +0700

    Huu: complete sign in

--- resources/views/admin/layout/nav.blade.php
+++ resources/views/admin/layout/nav.blade.php
@@ -1,3 +1,8 @@
+<style>
+    .dropdown .dropdown-menu.show:before {
+        top: 0px !important;
+    }
+</style>
 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
     <div class="container-fluid py-1 px-3">
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
@@ -6,19 +11,15 @@
             <ul class="navbar-nav  justify-content-end">
                 @if(Auth::check())
                 <li class="nav-item dropdown d-flex align-items-center">
-
                     <a class="nav-link text-white font-weight-bold px-0 dropdown active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="fa-solid fa-id-card me-sm-1"></i>
                         <span class="d-sm-inline d-none">{{ Auth::user()->fullName }}</span>
                     </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <ul class="dropdown-menu" style="top: -0.5rem!important;left: -55px;">
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li><a class="dropdown-item" href="/admin/sign_out">Sign Out</a></li>
                     </ul>
-
                 </li>
-
                 @endif
             </ul>
         </div>
