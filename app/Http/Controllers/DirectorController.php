49593716f34f82bcc8b03778601b1c67df2b406e
tqminh0907 <tqminh.0907@gmail.com>
2023-05-16 13:05:13 +0700

    zzxc

--- app/Http/Controllers/DirectorController.php
+++ app/Http/Controllers/DirectorController.php
@@ -5,10 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Director;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
-use Cloudinary\Api\Upload\UploadApi;
 use Cloudinary\Cloudinary;
-use Cloudinary\Transformation\Resize;
-use Cloudinary\Configuration\Configuration;
 
 class DirectorController extends Controller
 {
@@ -26,17 +23,6 @@ class DirectorController extends Controller
         ]);
         $file = $request->file('Image');
         $img = $request['image'] = $file;
-        Configuration::instance([
-            'cloud' => [
-                'cloud_name' => 'dgk9ztl5h',
-                'api_key' => '945974289843947',
-                'api_secret' => '9bEXv8Aoc9QY_CzjcOTrQrGlBHo'
-            ],
-            'url' => [
-                'secure' => false
-            ]
-        ]);
-        (new UploadApi())->upload('images/rated/C13.png');
     }
     public function postEdit()
     {
