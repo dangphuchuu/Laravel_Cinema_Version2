0050cfe54f29f50cdd29b6d58627ad6c3d3ab881
tqminh0907 <tqminh.0907@gmail.com>
2023-06-04 09:21:09 +0700

    Minh

--- resources/views/web/layout/index.blade.php
+++ resources/views/web/layout/index.blade.php
@@ -12,7 +12,9 @@
     <base href="{{asset('')}}">
 
     <!-- Bootstrap CSS -->
-    <link href="/web_assets/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css"
+          integrity="sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==" crossorigin="anonymous"
+          referrerpolicy="no-referrer"/>
 
     <!-- Fonts -->
     <!-- Font awesome - icon font -->
@@ -61,7 +63,9 @@
 <!-- JavaScript -->
 
 <!-- Bootstrap JS -->
-<script src="web_assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"
+        integrity="sha512-VK2zcvntEufaimc+efOYi622VN5ZacdnufnmX7zIhCPmjhKnOi9ZDMtg1/ug5l183f19gG1/cBstPO4D8N/Img==" crossorigin="anonymous"
+        referrerpolicy="no-referrer"></script>
 
 <!-- JQuery -->
 <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM="
