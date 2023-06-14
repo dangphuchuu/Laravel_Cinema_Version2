b1687d5d649d16fe3acd2e4aa5bb53fc6802ecea
tqminh0907 <tqminh.0907@gmail.com>
2023-06-14 14:15:31 +0700

    Huu

--- resources/views/web/layout/index.blade.php
+++ resources/views/web/layout/index.blade.php
@@ -78,12 +78,6 @@
 <!-- select2 -->
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
-
-<script>
-    $(document).ready(function () {
-        $('.js-example-basic-multiple').select2();
-    });
-</script>
 @yield('js')
 </body>
 
