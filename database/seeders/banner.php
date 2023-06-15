7041198ff13712b449906aeda6dd0468e80ea93a
tqminh0907 <tqminh.0907@gmail.com>
2023-06-15 13:59:46 +0700

    Minh

--- database/seeders/banner.php
+++ database/seeders/banner.php
@@ -4,16 +4,16 @@ use Illuminate\Support\Facades\DB;
 
 DB::table('banners')->insert([
     [
-        'name' => 'https://www.heyuguys.com/images/2012/11/Gangster-Squad-Banner.jpg',
+        'image' => 'https://www.heyuguys.com/images/2012/11/Gangster-Squad-Banner.jpg',
         'status' => 1,
     ], [
-        'name' => 'https://teaser-trailer.com/wp-content/uploads/Avengers-Infinity-War-Banner.jpg',
+        'image' => 'https://teaser-trailer.com/wp-content/uploads/Avengers-Infinity-War-Banner.jpg',
         'status' => 1,
     ], [
-        'name' => 'https://collider.com/wp-content/uploads/inception_movie_poster_banner_01.jpg',
+        'image' => 'https://collider.com/wp-content/uploads/inception_movie_poster_banner_01.jpg',
         'status' => 1,
     ], [
-        'name' => 'https://tedhicksfilmetc.files.wordpress.com/2013/06/world-war-z-banner-poster.jpg',
+        'image' => 'https://tedhicksfilmetc.files.wordpress.com/2013/06/world-war-z-banner-poster.jpg',
         'status' => 1,
     ]
 ]);
