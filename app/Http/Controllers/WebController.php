a2edb02db0b8338015e1d9f0665039188cb9db17
tqminh0907 <tqminh.0907@gmail.com>
2023-06-16 20:21:21 +0700

    Minh

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -136,6 +136,8 @@ class WebController extends Controller
                 'price' => $seat[2],
                 'ticket_id' => $ticket->id,
             ]);
+            $seatType = SeatType::where('row', $seat[0])->where('col', $seat[0])->where('room_id', $ticket->schedule->room_id)->get()->first();
+            $ticketSeat->seatType = $seatType->name;
             $ticketSeat->save();
         }
 
