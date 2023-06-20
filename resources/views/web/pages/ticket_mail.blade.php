18fc89fe754574b3ee0cd649ef327d0d78056492
tqminh0907 <tqminh.0907@gmail.com>
2023-06-20 22:05:28 +0700

    Minh

--- resources/views/web/pages/ticket_mail.blade.php
+++ resources/views/web/pages/ticket_mail.blade.php
@@ -1,18 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
-</head>
-<body>
-
 <div style="width: 600px;margin:0 auto">
-    <h2 style="text-align: center">Xin chào {!! $name !!}</h2>
+    <h2 style="text-align: center">Xin chào {{ $name }}</h2>
 </div>
 <div>
-    <img src="https://res.cloudinary.com/{!! $cloud_name !!}/image/upload/{!! $cloud !!}.png" alt="user1">
+    <img src="https://res.cloudinary.com/{{ cloud_name() }}/image/upload/{{ $cloud }}.png" alt="">
 </div>
-</body>
-</html>
