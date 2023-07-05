a67f1544e40975dea152fd0048c205f77d9f5bdd
tqminh0907 <tqminh.0907@gmail.com>
2023-07-05 11:42:11 +0700

    Minh

--- resources/views/web/layout/schedulesByTheater.blade.php
+++ resources/views/web/layout/schedulesByTheater.blade.php
@@ -1,11 +1,26 @@
 <div class="collapse @if($loop->first) show @endif" id="TheaterSchedules_{{$theater->id}}" data-bs-parent="#theaterSchedulesParent">
-    <div class="mt-5">
+    <ul class="list-group list-group-horizontal flex-wrap mt-4">
+        @for($i = 0; $i <= 7; $i++)
+            <li class="list-group-item border-0">
+                <button data-bs-toggle="collapse"
+                        data-bs-target="#schedule_{{$theater->id}}_date_{{$i}}"
+                        aria-expanded="false"
+                        class="btn btn-block btn-outline-dark p-2 m-2">
+                    {{ date('d/m', strtotime('+ '.$i.' day', strtotime(today()))) }}
+                </button>
+            </li>
+        @endfor
+    </ul>
+    <div class="mt-2">
         <h4>Lịch chiếu phim</h4>
         <div>
-            <div class="d-block mt-2 mb-5">
-                @foreach($movies as $movie)
+            <div class="d-block mt-2 mb-5"  id="schedulesMain_{{$theater->id}}">
+                @for($i = 0; $i <= 7; $i++)
+                    <div class="collapse collapse-horizontal @if($i == 0) show @endif" id="schedule_{{$theater->id}}_date_{{$i}}"
+                         data-bs-parent="#schedulesMain_{{$theater->id}}">
+                        @foreach($movies as $movie)
                     {{--                    {{ dd($movie->schedulesByDate($date_cur)) }}--}}
-                    @if($movie->schedulesByDateAndTheater($date_cur, $theater->id)->count() > 0)
+                    @if($movie->schedulesByDateAndTheater(date('Y-m-d', strtotime('+ '.$i.' day', strtotime(today()))), $theater->id)->count() > 0)
                         <div class="p-2 d-flex flex-row m-1 align-items-center rounded" style="background: #f5f5f5">
                             <div class="flex-shrink-0 p-2 border-end border-4 border-white">
                                 @if(strstr($movie->image,"https") === "")
@@ -18,11 +33,11 @@
                             {{-- a Theater schedule --}}
                             <div class="flex-grow-1 border-start border-5 border-white p-2 ps-4">
                                 @foreach($roomTypes as $roomType)
-                                    @if($roomType->schedulesByDateAndTheaterAndMovie($date_cur, $theater->id, $movie->id)->count() > 0)
+                                    @if($roomType->schedulesByDateAndTheaterAndMovie(date('Y-m-d', strtotime('+ '.$i.' day', strtotime(today()))), $theater->id, $movie->id)->count() > 0)
                                         <div class="d-flex flex-column flex-nowrap overflow-auto mb-4">
                                             <div class="fw-bold">{{ $roomType->name }}</div>
                                             <div class="d-flex flex-wrap overflow-wrapper">
-                                                @foreach($roomType->schedulesByDateAndTheaterAndMovie($date_cur, $theater->id, $movie->id) as $schedule)
+                                                @foreach($roomType->schedulesByDateAndTheaterAndMovie(date('Y-m-d', strtotime('+ '.$i.' day', strtotime(today()))), $theater->id, $movie->id) as $schedule)
                                                     @if(date('Y-m-d') == $schedule->date)
                                                         @if(date('H:i', strtotime('+ 20 minutes', strtotime($schedule->startTime))) >= date('H:i'))
                                                             @if(Auth::check())
@@ -75,6 +90,8 @@
                         </div>
                     @endif
                 @endforeach
+                    </div>
+                @endfor
             </div>
         </div>
     </div>
