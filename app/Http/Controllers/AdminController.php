c61ba126394a42b123a3cfe4792bc9b9d8a2a208
dangphuchuu <phuchuu0120@gmail.com>
2023-07-10 22:15:01 +0700

    huu

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,11 +18,6 @@ class AdminController extends Controller
 {
     function __construct()
     {
-        // Return an instance of the Auth component for the default Firebase project
-        $defaultAuth = Firebase::auth();
-// Return an instance of the Auth component for a specific Firebase project
-        $appAuth = Firebase::project('app')->auth();
-        $anotherAppAuth = Firebase::project('another-app')->auth();
     }
 
     public function home(Request $request)
