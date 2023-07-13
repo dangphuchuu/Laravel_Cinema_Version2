5befaf331547e1ae6ebfe41437c661ced8403d1f
dangphuchuu <phuchuu0120@gmail.com>
2023-07-13 14:17:55 +0700

    fix bug

--- resources/views/web/pages/ticket_mail.blade.php
+++ resources/views/web/pages/ticket_mail.blade.php
@@ -1,5 +1,6 @@
 <div style="width: 600px; margin:0 auto">
     <h2 style="text-align: center">Xin chào {{ $name }}</h2>
+    <h3 style="text-align: center"> dưới đây là thông tin vé của bạn vui lòng kiểm tra và mang vé cho nhân viên để vào rạp.</h3>
 </div>
 <div style="width: 600px; margin:0 auto">
     <img src="https://res.cloudinary.com/{{ $cloud_name }}/image/upload/{{ $cloud }}.png" alt="">
