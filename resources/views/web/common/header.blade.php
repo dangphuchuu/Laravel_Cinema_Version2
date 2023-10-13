a9cad119475bb7e044c833ff8b7c82f1d58dd903
tqminh0907 <tqminh.0907@gmail.com>
2023-10-13 13:32:47 +0700

    minh

--- resources/views/web/common/header.blade.php
+++ resources/views/web/common/header.blade.php
@@ -74,43 +74,4 @@
         </div>
       </div>
     </div>
-
-
-
-    <a class="navbar-brand ms-4 position-absolute top-50 start-50 translate-middle d-flex flex-column" href="#">
-      <div class="d-flex justify-content-center">
-        <img src="images/favicon/cinema.png" height="40px" width="auto" alt="Logo" class="d-block">
-      </div>
-      <span class="">{{isset($info['name']) ? $info['name'] : 'Cinema'}}</span>
-    </a>
-
-
-
-    @if(Auth::check())
-    <div class="nav-item dropdown d-flex justify-content-center float-lg-end mx-2 my-2 my-lg-0">
-      <a class="nav-link link-light dropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-        <i class="fa-solid fa-circle-user fa-2xl mx-2"></i>
-        <span class="text-nowrap">{{ Auth::user()->fullName }}</span>
-        <i class="fa-regular fa-chevron-down mx-2 fa-sm"></i>
-      </a>
-      <ul class="dropdown-menu shadow border border-dark mt-3 px-4 text-center" style="background-color: #020817">
-        <li>
-          <a class="link link-light dropdown-item link-underline-opacity-100-hover" href="/profile">@lang('lang.profile')</a>
-        </li>
-        <li>
-          <a class="link link-light dropdown-item link-underline-opacity-100-hover" href="/signOut">@lang('lang.signout')</a>
-        </li>
-      </ul>
-    </div>
-    @else
-    <div class="d-flex justify-content-center float-lg-end my-2 my-lg-0">
-      <button class="btn btn-outline-light rounded-pill px-4 m-2 text-nowrap" type="button" href="#registerModal" data-bs-toggle="modal" data-bs-target="#registerModal">
-        @lang("lang.signup")
-      </button>
-      <button class="btn btn-danger rounded-pill ms-2 px-4 m-2 text-nowrap" type="button" href="#loginModal" data-bs-toggle="modal" data-bs-target="#loginModal">
-        @lang("lang.signin")
-      </button>
-    </div>
-    @endif
-  </div>
 </nav>
